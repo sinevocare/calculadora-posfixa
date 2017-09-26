ed5bf73072691e8fa3938493f0b02ee5680aef8d
yuri <yurijrdn@gmail.com>
2017-09-26 09:15:56 -0300

    Teste ignore

---
+++ src/CalculadoraPosFixa.java
@@ -0,0 +1,7 @@
+
+class CalculadoraPosFixa {
+
+    public CalculadoraPosFixa() {
+    }
+    
+}
---
+++ test/CalculadoraPosFixaTeste.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+
+public class CalculadoraPosFixaTeste {
+    
+    CalculadoraPosFixa cpf;
+    
+    @Before
+    public void setUp(){
+        cpf =  new CalculadoraPosFixa();
+    }
+    
+    @Test
+    public void c(){
+        assertTrue(true);
+    }
+}
