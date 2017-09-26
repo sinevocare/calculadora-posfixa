eeb9ea99b7acf58db2d208b5af06c0f2707c4595
yuri <yurijrdn@gmail.com>
2017-09-26 13:17:06 -0300

    Plano de teste finalizado

--- src/CalculadoraPosFixa.java
+++ src/CalculadoraPosFixa.java
@@ -1,10 +1,8 @@
 /*
  * A implementação da calculadora será baseada na estrutura de dados: PILHA.
  */
-import java.util.Arrays;
+
 import java.util.Stack;
-
-
 
 class CalculadoraPosFixa {
 
@@ -22,8 +20,7 @@ class CalculadoraPosFixa {
         return this.entradas.isEmpty();
     }
 
-    public void empilha(String entrada) {
-
+    public void empilhar(String entrada) {
         this.entradas.push(entrada);
 
     }
@@ -35,9 +32,14 @@ class CalculadoraPosFixa {
     }
 
     public String exibirExpressao() {
-        
       
         return String.join(" ", this.entradas);
     }
+
+    public String resolverExpressao() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
 
 }
--- test/CalculadoraPosFixaTeste.java
+++ test/CalculadoraPosFixaTeste.java
@@ -19,64 +19,173 @@ public class CalculadoraPosFixaTeste {
     public void calculadoraVazia() {
         assertTrue(cpf.estaVazia());
         assertEquals(cpf.getTamanho(), 0);
+        assertEquals("", cpf.exibirExpressao());
     }
 
+    /*
+     * ===========================================================================   
+     * TESTES APENAS INSERÇÃO DE OPERANDOS
+     * ===========================================================================
+     */
     @Test
     public void acrescentar1Operando() {
 
-        cpf.empilha("5");
+        cpf.empilhar("5");
 
         assertFalse(cpf.estaVazia());
         assertEquals(cpf.getTamanho(), 1);
         assertEquals("5", cpf.topo());
+        assertEquals("5", cpf.exibirExpressao());
     }
-    
+
     @Test
     public void acrescentar2Operandos() {
 
-        cpf.empilha("5");
-        cpf.empilha("2");
-        
+        cpf.empilhar("5");
+        cpf.empilhar("2");
+
         assertFalse(cpf.estaVazia());
         assertEquals(cpf.getTamanho(), 2);
         assertEquals("2", cpf.topo());
+        assertEquals("5 2", cpf.exibirExpressao());
     }
-    
+
     @Test
     public void acrescentarOperandoComMaisDeUmAlgarismo() {
 
-        cpf.empilha("50");
-        
+        cpf.empilhar("50");
+
         assertFalse(cpf.estaVazia());
         assertEquals(cpf.getTamanho(), 1);
         assertEquals("50", cpf.topo());
+        assertEquals("50", cpf.exibirExpressao());
     }
-    
-    
-    @Test
-    public void exibeDoisOperandosAcrescentados() {
 
-        cpf.empilha("5");
-        cpf.empilha("2");
-        
+    @Test
+    public void exibir3OperandosAcrescentados() {
+
+        cpf.empilhar("5");
+        cpf.empilhar("2");
+        cpf.empilhar("50");
+
         assertFalse(cpf.estaVazia());
-        assertEquals(cpf.getTamanho(), 2);
-        assertEquals("5 2", cpf.exibirExpressao());
+        assertEquals(cpf.getTamanho(), 3);
+        assertEquals("5 2 50", cpf.exibirExpressao());
     }
-    
-     @Test
+
+    @Test
     public void acrescentar5Operandos() {
 
-        cpf.empilha("5");
-        cpf.empilha("2");
-        cpf.empilha("5");
-        cpf.empilha("2");
-        cpf.empilha("10");
-        
+        cpf.empilhar("5");
+        cpf.empilhar("2");
+        cpf.empilhar("5");
+        cpf.empilhar("2");
+        cpf.empilhar("10");
+
         assertFalse(cpf.estaVazia());
         assertEquals(cpf.getTamanho(), 5);
         assertEquals("5 2 5 2 10", cpf.exibirExpressao());
         assertEquals("10", cpf.topo());
     }
+
+    /*
+     * ===========================================================================   
+     * TESTES COM OPERADORES
+     * ===========================================================================
+     */
+    @Test(expected = CalculadoraPosFixaException.class)
+    public void entradaSemOperandos() {
+
+        cpf.empilhar("+");
+        cpf.exibirExpressao();
+
+    }
+
+    @Test
+    public void soma2Operandos() {
+
+        cpf.empilhar("5");
+        cpf.empilhar("2");
+        cpf.empilhar("+");
+
+        assertFalse(cpf.estaVazia());
+        assertEquals(cpf.getTamanho(), 2);
+        assertEquals("2", cpf.topo());
+        assertEquals("5 2 +", cpf.exibirExpressao());
+        assertEquals("7", cpf.resolverExpressao());
+
+    }
+
+    @Test
+    public void subtracao2Operandos() {
+
+        cpf.empilhar("5");
+        cpf.empilhar("2");
+        cpf.empilhar("-");
+
+        assertFalse(cpf.estaVazia());
+        assertEquals(cpf.getTamanho(), 2);
+        assertEquals("2", cpf.topo());
+        assertEquals("5 2 -", cpf.exibirExpressao());
+        assertEquals("3", cpf.resolverExpressao());
+
+    }
+
+    @Test
+    public void multiplicacao2Operandos() {
+
+        cpf.empilhar("5");
+        cpf.empilhar("2");
+        cpf.empilhar("*");
+
+        assertFalse(cpf.estaVazia());
+        assertEquals(cpf.getTamanho(), 2);
+        assertEquals("2", cpf.topo());
+        assertEquals("5 2 *", cpf.exibirExpressao());
+        assertEquals("10", cpf.resolverExpressao());
+
+    }
+
+    @Test
+    public void divisao2Operandos() {
+
+        cpf.empilhar("5");
+        cpf.empilhar("2");
+        cpf.empilhar("/");
+
+        assertFalse(cpf.estaVazia());
+        assertEquals(cpf.getTamanho(), 2);
+        assertEquals("2", cpf.topo());
+        assertEquals("5 2 /", cpf.exibirExpressao());
+        assertEquals("2.5", cpf.resolverExpressao());
+
+    }
+
+    @Test(expected = CalculadoraPosFixaException.class)
+    public void divisaoPorZero() {
+
+        cpf.empilhar("5");
+        cpf.empilhar("0");
+        cpf.empilhar("/");
+
+        cpf.resolverExpressao();
+
+    }
     
+    @Test
+    public void expressaoComVariosOperandos() {
+
+        cpf.empilhar("5");
+        cpf.empilhar("2");
+        cpf.empilhar("-");
+        cpf.empilhar("2");
+        cpf.empilhar("*");
+        
+        assertFalse(cpf.estaVazia());
+        assertEquals(cpf.getTamanho(), 5);
+        assertEquals("*", cpf.topo());
+        assertEquals("5 2 - 2 *", cpf.exibirExpressao());
+        assertEquals("6", cpf.resolverExpressao());
+    }
+
 }
