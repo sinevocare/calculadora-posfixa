386bfc76c79529312a9b7c0a07184bf77a68c859
yuri <yurijrdn@gmail.com>
2017-09-26 11:12:01 -0300

    Teste com acréscimo de operandos implementados

--- src/CalculadoraPosFixa.java
+++ src/CalculadoraPosFixa.java
@@ -1,7 +1,43 @@
+/*
+ * A implementação da calculadora será baseada na estrutura de dados: PILHA.
+ */
+import java.util.Arrays;
+import java.util.Stack;
+
+
 
 class CalculadoraPosFixa {
 
+    private Stack entradas;
+
     public CalculadoraPosFixa() {
+        entradas = new Stack();
     }
-    
+
+    public int getTamanho() {
+        return this.entradas.size();
+    }
+
+    public boolean estaVazia() {
+        return this.entradas.isEmpty();
+    }
+
+    public void empilha(String entrada) {
+
+        this.entradas.push(entrada);
+
+    }
+
+    public String topo() {
+
+        return this.entradas.peek().toString();
+
+    }
+
+    public String exibirExpressao() {
+        
+      
+        return String.join(" ", this.entradas);
+    }
+
 }
--- test/CalculadoraPosFixaTeste.java
+++ test/CalculadoraPosFixaTeste.java
@@ -1,25 +1,82 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Os teste serão executados, baseados nas operações da estrutura de dados: PILHA.
  */
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
 
-
 public class CalculadoraPosFixaTeste {
-    
+
     CalculadoraPosFixa cpf;
-    
+
     @Before
-    public void setUp(){
-        cpf =  new CalculadoraPosFixa();
+    public void setUp() {
+        cpf = new CalculadoraPosFixa();
+    }
+
+    @Test
+    public void calculadoraVazia() {
+        assertTrue(cpf.estaVazia());
+        assertEquals(cpf.getTamanho(), 0);
+    }
+
+    @Test
+    public void acrescentar1Operando() {
+
+        cpf.empilha("5");
+
+        assertFalse(cpf.estaVazia());
+        assertEquals(cpf.getTamanho(), 1);
+        assertEquals("5", cpf.topo());
     }
     
     @Test
-    public void c(){
-        assertTrue(true);
+    public void acrescentar2Operandos() {
+
+        cpf.empilha("5");
+        cpf.empilha("2");
+        
+        assertFalse(cpf.estaVazia());
+        assertEquals(cpf.getTamanho(), 2);
+        assertEquals("2", cpf.topo());
     }
+    
+    @Test
+    public void acrescentarOperandoComMaisDeUmAlgarismo() {
+
+        cpf.empilha("50");
+        
+        assertFalse(cpf.estaVazia());
+        assertEquals(cpf.getTamanho(), 1);
+        assertEquals("50", cpf.topo());
+    }
+    
+    
+    @Test
+    public void exibeDoisOperandosAcrescentados() {
+
+        cpf.empilha("5");
+        cpf.empilha("2");
+        
+        assertFalse(cpf.estaVazia());
+        assertEquals(cpf.getTamanho(), 2);
+        assertEquals("5 2", cpf.exibirExpressao());
+    }
+    
+     @Test
+    public void acrescentar5Operandos() {
+
+        cpf.empilha("5");
+        cpf.empilha("2");
+        cpf.empilha("5");
+        cpf.empilha("2");
+        cpf.empilha("10");
+        
+        assertFalse(cpf.estaVazia());
+        assertEquals(cpf.getTamanho(), 5);
+        assertEquals("5 2 5 2 10", cpf.exibirExpressao());
+        assertEquals("10", cpf.topo());
+    }
+    
 }
